86e2d4b693c6cf764d477293db9d783c0f0e6158
Alberto Rodríguez <alberto.rodriguezrdgz@gmail.com>
2026-02-07 19:32:44 +0100

    initialize Spring Boot project

---
+++ src/main/java/com/alberto/clientes_cuentas_microservicios/ClientesCuentasMicroserviciosApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.clientes_cuentas_microservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesCuentasMicroserviciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesCuentasMicroserviciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alberto/clientes_cuentas_microservicios/ClientesCuentasMicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.clientes_cuentas_microservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesCuentasMicroserviciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
