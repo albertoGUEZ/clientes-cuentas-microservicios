af505256b73486821f567ad55389b7bbbe672c8d
Alberto Rodríguez <alberto.rodriguezrdgz@gmail.com>
2026-02-08 16:37:04 +0100

    feature/Define repository ports for Cliente and CuentaBancaria domain persistence

---
+++ src/main/java/com/alberto/clientes_cuentas_microservicios/application/port/out/ClienteRepositoryPort.java
@@ -0,0 +1,20 @@
+package com.alberto.clientes_cuentas_microservicios.application.port.out;
+
+import com.alberto.clientes_cuentas_microservicios.domain.model.Cliente;
+import com.alberto.clientes_cuentas_microservicios.domain.model.Dni;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteRepositoryPort {
+
+    List<Cliente> findAll();
+
+    List<Cliente> findAllAdults();
+
+    List<Cliente> findAllWithTotalAccountGreaterThan(double total);
+
+    Optional<Cliente> findByDni(Dni dni);
+
+    Cliente save(Cliente cliente);
+}
