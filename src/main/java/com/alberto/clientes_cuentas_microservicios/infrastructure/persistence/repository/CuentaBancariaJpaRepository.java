57fb6d662b33c0bf7ecdc132740fd4cc1e181756
Alberto Rodríguez <alberto.rodriguezrdgz@gmail.com>
2026-02-10 02:33:43 +0100

    feature: Update README.md with project overview, installation instructions, and technology stack

--- src/main/java/com/alberto/clientes_cuentas_microservicios/infrastructure/persistence/repository/CuentaBancariaJpaRepository.java
+++ src/main/java/com/alberto/clientes_cuentas_microservicios/infrastructure/persistence/repository/CuentaBancariaJpaRepository.java
@@ -2,9 +2,11 @@ package com.alberto.clientes_cuentas_microservicios.infrastructure.persistence.r
 
 import com.alberto.clientes_cuentas_microservicios.infrastructure.persistence.entity.CuentaBancariaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CuentaBancariaJpaRepository extends JpaRepository<CuentaBancariaEntity, Long> {
 
     List<CuentaBancariaEntity> findByDniCliente(String dniCliente);
